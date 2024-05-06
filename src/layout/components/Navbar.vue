43cabcf858ec7378abcef8a330a726ca7c5452be
lxb <1483628145@qq.com>
2024-05-06 12:18:10 +0800

    删除多余模块

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -71,7 +71,7 @@ export default {
   },
   data() {
     return {
-      centerDialogVisible: true,
+      centerDialogVisible: false,
       updateUserForm: {
         oldPassword: '',
         newPassword: '',
